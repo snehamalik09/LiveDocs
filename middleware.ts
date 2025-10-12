c10050323049153386edb9bf161f3ce2deea1a6c
Sneha Malik <snehamalik962003@gmail.com>
2025-10-12 23:38:02 +0530

    webhook setup

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)', '/']);
 
-export default clerkMiddleware();
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
